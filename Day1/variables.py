685e7664b286d7fd5484a4d5e1c6763493bd4e45
Denver Timlick <denvertimlick@gmail.com>
2025-10-29 15:02:41 -0600

    created to repo for 100 days of pyhton project
    added DAy1 and Day2 files

---
+++ Day1/variables.py
@@ -0,0 +1,12 @@
+# name = input("What is your name? ")
+# print(name)
+
+# name = 'Angela'
+# print(len(name))
+
+# print(len(input("What is your name? ")))
+
+
+username = input("What is your name? ")
+usernameLegnth = len(username)
+print(usernameLegnth)
