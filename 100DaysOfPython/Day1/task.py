53bac50ee442205c31f9e0f21a7e705f82d0e6b9
Denver Timlick <denvertimlick@gmail.com>
2025-10-29 16:48:21 -0600

    Flattened nested repo: 100DaysOfPython part of PythonLearning

---
+++ 100DaysOfPython/Day1/task.py
@@ -0,0 +1,3 @@
+# This line of code will take an input using the input() funtion
+# it then replaces the input() with user input then prints the full statment
+print("Hello " + input("What is your name? ") + "!")
