c9f78cbf83e4a2d4f4338230084626c3c2621fc1
Denver Timlick <denvertimlick@gmail.com>
2026-04-25 15:03:13 -0600

    Day 2 progress

---
+++ Day2/PrimitiveData.py
@@ -0,0 +1,20 @@
+# Subscripting
+print("Hello"[-1])
+
+# String concatenation
+print("123" + "456")
+
+
+# Integer = Whole NUmber
+print(123 + 456)
+
+
+# Large Integers
+print(123_456_789)
+
+# Float = Floating Point Number
+print(3.14159)
+
+# Boolean
+print(True)
+print(False)
---
+++ Day2/TypeChecking.py
@@ -0,0 +1,8 @@
+len("Hello")
+
+print(type(123))
+print(type("Hello"))
+print(type(3.14159))
+print(type(True))
+
+print(int("123") + int("456"))
