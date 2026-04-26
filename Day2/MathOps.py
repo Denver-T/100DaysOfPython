2a41bcbac228f0a3504e519fb760db4ce4dd0ce2
Denver Timlick <denvertimlick@gmail.com>
2026-04-26 09:49:03 -0600

    Day 2 completed

---
+++ Day2/MathOps.py
@@ -0,0 +1,30 @@
+# print("My age: " + str(12))
+# print(123 + 456)
+# print(7 - 5)
+# print(3 * 2)
+# print(5 / 3)
+# print(5 // 3)
+# print(2**3)
+
+# PEMDAS
+# Parentheses, Exponents, Multiplication/Division, Addition/Subtraction
+
+print(3 * 3 + 3 / 3 - 3)
+
+height = 1.65
+weight = 84
+
+bmi = weight / height**2
+print(bmi)
+
+print(int(bmi))
+print(round(bmi, 2))
+
+score = 0
+
+# User scores a point
+score += 1
+height = 1.8
+is_winning = True
+
+print(f"Your score is {score}, your height is {height}, are you winning? {is_winning}")
--- Day2/TypeChecking.py
+++ Day2/TypeChecking.py
@@ -6,3 +6,10 @@ print(type(3.14159))
 print(type(True))
 
 print(int("123") + int("456"))
+
+int()
+float()
+str()
+bool()
+
+print("Numbers of letters in your name: " + str(len(input("Enter your name:"))))
