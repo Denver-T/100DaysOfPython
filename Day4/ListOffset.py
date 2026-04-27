996daa1a393851086eb4c8ea6560e20e150f4031
Denver Timlick <denvertimlick@gmail.com>
2026-04-27 12:39:15 -0600

    Day 4 completed

---
+++ Day4/ListOffset.py
@@ -0,0 +1,22 @@
+# import random
+
+states_of_america = [
+    "Delaware",
+    "Pannsylvania",
+    "New Jersey",
+    "Georgia",
+]
+
+# states_of_america.append("Connecticut")
+
+# states_of_america.extend(["Massachusetts", "Maryland"])
+num_of_states = len(states_of_america)
+print(states_of_america[num_of_states - 1])
+
+# friends = ["Alice", "Bob", "Charlie", "David", "Emanuel"]
+# # Option 1
+# print(random.choice(friends))
+
+# # Option 2
+# random_index = random.randint(0, 4)
+# print(friends[random_index])
